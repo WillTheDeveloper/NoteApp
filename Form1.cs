1c978a1af275269e8fd79ecbb25711510975ed75
Will <twitchifygamers@gmail.com>
2021-12-13 15:10:28 +0000

    Added write file code to save button

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace NoteApp
@@ -19,7 +20,10 @@ namespace NoteApp
 
         private void savebutton_Click(object sender, EventArgs e)
         {
+            string Note = notelabel.Text;
+            string Title = titlelabel.Text;
 
+            File.WriteAllText(Title + ".txt", Note);
         }
     }
 }
